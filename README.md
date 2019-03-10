cbae9e19350f8b5d6b8ab371bc0d565a17266e96
emmyabba <emmyabba@gmail.com>
2019-03-10 09:48:57 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Larave 5.8 Multi Auth system by @greatnexx
+#Fresh laravel 5.8 installation 
+<<<<<<< HEAD
+Cause trouble
+=======
+Conflict
+>>>>>>> very-bad
---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    public function index()
+    {
+        return view('admin.home');
+    }
+}
